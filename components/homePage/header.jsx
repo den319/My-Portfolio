6d3c34d9cf26ba8d1fb7ab9afb098bf30e6e6954
Den <den@localhost.localdomain>
2024-03-02 17:03:32 +0530

    first commit

---
+++ components/homePage/header.jsx
@@ -0,0 +1,10 @@
+
+"use client"
+
+export default function Header() {
+    return (
+        <div className = "flex w-full text-center justify-center text-[33px]">
+            Hello World
+        </div>
+    )
+}
